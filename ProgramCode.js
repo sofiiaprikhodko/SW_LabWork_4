9a347c8ac7446d81d331e6b172fc32adcf733a11
Sofiia Prykhodko <162308414+sofiiaprikhodko@users.noreply.github.com>
2025-11-04 21:13:27 +0200

    Create ProgramCode.js

---
+++ ProgramCode.js
@@ -0,0 +1,10 @@
+const PROGRAM_NAME = 'Система створення скульптур';
+
+/**
+ * Основна функція, яка запускає виведення назви в термінал.
+ */
+function displayProgramName() {
+  console.log(`Програмний продукт: ${PROGRAM_NAME}`);
+}
+
+displayProgramName();
